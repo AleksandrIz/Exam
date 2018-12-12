3f7bf1e19b895382058f4d965dc1db71cca6cd6c
AleksandrIz <sashaizik@gmail.com>
2018-12-12 11:13:20 +0200

    layout header

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// плавный переход вверх
+
+$(document).ready(function(){
+	$("#rocketFixed").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
